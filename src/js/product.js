f3d21d74511e91e5204b417d5019511c6a339ab4
Eduardo Pulido <pul21010@byui.edu>
2024-09-23 17:47:49 -0500

    additional fixes associated with prettierrc

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  setLocalStorage('so-cart', product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-   // Check if the target has a data-id attribute
-  
+  // Check if the target has a data-id attribute
+
   const product = await dataSource.findProductById(e.target.dataset.id);
   const productId = e.target.dataset.id;
   if (!productId) {
-    console.error("Product ID not found on the clicked element.");
+    console.error('Product ID not found on the clicked element.');
     return; // Exit if no product ID is present
   }
   addProductToCart(product);
@@ -21,5 +21,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
