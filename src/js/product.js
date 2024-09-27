17dd0b1285c769bedb3a6c10e8cc16d88bc5bb58
Eduardo Pulido <pul21010@byui.edu>
2024-09-27 18:03:53 -0500

    changes after branch creation

--- src/js/product.js
+++ src/js/product.js
@@ -7,25 +7,4 @@ const dataSource = new ProductData('tents');
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-// console.log(datasource.findProductById(product));
 
-/* function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  // Check if the target has a data-id attribute
-
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  const productId = e.target.dataset.id;
-  if (!productId) {
-    console.error('Product ID not found on the clicked element.');
-    return; // Exit if no product ID is present
-  }
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler); */
