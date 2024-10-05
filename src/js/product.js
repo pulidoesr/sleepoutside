1d1bc8ed539f5e28216c2f22df27e45232d94f81
Eduardo Pulido <pul21010@byui.edu>
2024-10-05 12:51:28 -0500

    w03 product page header and footer correction

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,10 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
 import { setLocalStorage, getParams } from './utils.mjs';
+
+// load Header and Footer
+loadHeaderFooter();
 
 const productId = getParams('product');
 const dataSource = new ProductData('tents');
